f6885b86e81d4a4888b3b10e5a147bc1331da9c0
HaroldF415 <harold.f415@gmail.com>
2022-11-22 19:15:02 -0500

    .findPinkElephantsByTimestreet() complete.

--- index.js
+++ index.js
@@ -81,7 +81,9 @@ function getTitleAndArtist(songs) {
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  return songs.find( song => song.title === 'Pink Elephants');
+};
 
 /**
  * Returns the first song in the list that is under three minutes.
